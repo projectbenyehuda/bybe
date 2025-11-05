4f1405823ad560f1c14b746084813bdabc555f89
Asaf Bartov <asaf.bartov@gmail.com>
2025-11-05 17:14:30 +0200

    Use MarkdownToHtml more consistently

--- app/services/markdown_to_html.rb
+++ app/services/markdown_to_html.rb
@@ -5,14 +5,15 @@ class MarkdownToHtml < ApplicationService
   def call(markdown)
     return '' if markdown.blank?
 
+    # remove MMD's automatic figcaptions
     html = MultiMarkdown.new(markdown).to_html.force_encoding('UTF-8')
-                        .gsub(%r{<figcaption>.*?</figcaption>}, '') # remove MMD's automatic figcaptions
-
-    # Change first <p> element in footnotes to <span> to prevent line break
+                        .gsub(%r{<figcaption>.*?</figcaption>}, '')
+                        .gsub('<table>', '<div style="overflow-x:auto;"><table>')
+                        .gsub('</table>', '</table></div>')
     html.gsub!(%r{(<li id="fn:\d+"[^>]*>\s*)<p>(.*?)</p>}) do
+      # Change first <p> element in footnotes to <span> to prevent line break
       "#{::Regexp.last_match(1)}<span>#{::Regexp.last_match(2)}</span>"
     end
-
     html
   end
 end
