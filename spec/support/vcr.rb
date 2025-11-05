195ffab23cb31848489a2591e870f407fe086e3f
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-11-06 03:01:41 +0500

    Added service to parse citaitons using DeepSeek

---
+++ spec/support/vcr.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "#{::Rails.root}/spec/fixtures/cassettes"
+  config.ignore_localhost = true
+  config.configure_rspec_metadata!
+  config.hook_into :webmock
+  %w(DEEPSEEK_API_KEY).each do |placeholder|
+    config.filter_sensitive_data(placeholder) { ENV.fetch(placeholder) }
+  end
+end
