4ae3d6a27a1a26574f9222b0ef24ec31a76ae805
Asaf Bartov <asaf.bartov@gmail.com>
2025-11-05 16:51:56 +0200

    Fix footnotes placement in all browsers

--- app/services/markdown_to_html.rb
+++ app/services/markdown_to_html.rb
@@ -5,7 +5,14 @@ class MarkdownToHtml < ApplicationService
   def call(markdown)
     return '' if markdown.blank?
 
-    MultiMarkdown.new(markdown).to_html.force_encoding('UTF-8')
-                 .gsub(%r{<figcaption>.*?</figcaption>}, '') # remove MMD's automatic figcaptions
+    html = MultiMarkdown.new(markdown).to_html.force_encoding('UTF-8')
+                        .gsub(%r{<figcaption>.*?</figcaption>}, '') # remove MMD's automatic figcaptions
+
+    # Change first <p> element in footnotes to <span> to prevent line break
+    html.gsub!(%r{(<li id="fn:\d+"[^>]*>\s*)<p>(.*?)</p>}) do
+      "#{::Regexp.last_match(1)}<span>#{::Regexp.last_match(2)}</span>"
+    end
+
+    html
   end
 end
