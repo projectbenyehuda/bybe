71f42173781ccbf7aaaeafd7826214ab64a95778
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-11-11 03:33:39 +0500

    switched to OpenAI from DeepSeek

--- db/schema.rb
+++ db/schema.rb
@@ -704,7 +704,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_18_200936) do
     t.string "deathdate"
     t.text "bio"
     t.text "works"
-    t.text "about"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.integer "authority_id"
