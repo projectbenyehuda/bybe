dea97ff918aeb3630e20efa2ac25cace790a4b2d
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-12-01 22:02:00 +0500

    Extracted newsfeed generation logic into a Newsfeed service. Updated welcome page to cache rendered newsfeed HTML instead of array of NewsItem records (they were restored from cache with empty authority_id as authority_id is transient attribute)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -252,64 +252,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def cached_newsfeed
-    Rails.cache.fetch('cached_newsfeed', expires_in: 1.hour) do # memoize
-      newsfeed
-    end
-  end
-
-  def cached_youtube_videos
-    Rails.cache.fetch('cached_youtube', expires_in: 24.hours) do # memoize
-      # return latest_youtube_videos # commented out due to quote problem, and caching failure yet TBD
-      []
-    end
-  end
-
-  def latest_youtube_videos
-    ret = []
-    begin
-      channel = Yt::Channel.new id: Rails.configuration.constants['youtube_channel_id']
-      vids = channel.videos
-      max = vids.count > 5 ? 5 : vids.count
-      i = 0
-      vids.each  do |v|
-        break if i >= max
-
-        ret << [v.title, v.description, v.id, v.thumbnail_url, v.published_at]
-        i += 1
-      end
-    rescue StandardError
-      puts 'No network?'
-    end
-    return ret
-  end
-
-  def youtube_url_from_id(id)
-    return 'https://www.youtube.com/watch?v=' + id
-  end
-
   public # temp
-
-  def newsfeed
-    unsorted_news_items = NewsItem.last(5) # read at most the last 5 persistent news items (Facebook posts, announcements)
-
-    WhatsNewSince.call(1.month.ago).each do |person, pubs| # add newly-published works
-      # Extract manifestations from pubs hash (skip :latest key)
-      manifestations = pubs.values.select { |v| v.is_a?(Array) }.flatten
-      unsorted_news_items << NewsItem.from_publications(
-        person,
-        TextifyNewPubs.call(manifestations),
-        pubs,
-        authority_path(person.id),
-        person.profile_image.url(:thumb)
-      )
-    end
-    cached_youtube_videos.each do |title, desc, id, thumbnail_url, relevance| # add latest videos
-      unsorted_news_items << NewsItem.from_youtube(title, desc, youtube_url_from_id(id), thumbnail_url, relevance)
-    end
-    # TODO: add latest blog posts
-    return unsorted_news_items.sort_by { |item| item.relevance }.reverse # sort by descending relevance
-  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
