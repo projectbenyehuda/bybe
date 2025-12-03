c31eda92dd135b1d879a6b8cd942744f9fadd906
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-12-03 22:16:54 +0500

    removed ParseAgain action and ParseCitation service. We only rely on LLM parsing now

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,6 @@ Bybeconv::Application.routes.draw do
       resources :citations, shallow: true, except: %i(show) do
         member do
           post :approve
-          post :parse_again
         end
       end
     end
