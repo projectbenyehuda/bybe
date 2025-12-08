942f607fab61fece69920193e901540578f988ec
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2025-12-08 18:20:06 +0500

    Refactored LexCitation: - made lex_person_id mandatory and main way to link citation to a person page. - made item optional and only used for LexPublications (replace with optional lex_publication_id?) - refactored UI to use this approach

--- app/models/lex_publication.rb
+++ app/models/lex_publication.rb
@@ -2,9 +2,9 @@
 
 # Lexicon entry about publication
 class LexPublication < ApplicationRecord
-  include Lexicon::WithCitations
-
   has_one :entry, as: :lex_item, class_name: 'LexEntry', dependent: :destroy
+
+  has_many :citations, as: :item, inverse_of: :item, class_name: 'LexCitation', dependent: :destroy
 
   accepts_nested_attributes_for :entry, update_only: true
 end
