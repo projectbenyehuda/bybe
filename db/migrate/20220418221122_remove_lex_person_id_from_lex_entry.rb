979aa0111689ca762fd8d20a621b8ec39c686494
Asaf Bartov <asaf.bartov@gmail.com>
2025-12-10 05:03:03 +0200

    Made migrations tolerant of existing changes

--- db/migrate/20220418221122_remove_lex_person_id_from_lex_entry.rb
+++ db/migrate/20220418221122_remove_lex_person_id_from_lex_entry.rb
@@ -1,6 +1,6 @@
 class RemoveLexPersonIdFromLexEntry < ActiveRecord::Migration[5.2]
   def change
-    remove_column :lex_entries, :lex_person_id, :bigint
-    remove_column :lex_entries, :lex_publication_id, :bigint
+    remove_column :lex_entries, :lex_person_id, :bigint, if_exists: true
+    remove_column :lex_entries, :lex_publication_id, :bigint, if_exists: true
   end
 end
--- db/migrate/20250925100631_remove_root_collection_id_from_authorities.rb
+++ db/migrate/20250925100631_remove_root_collection_id_from_authorities.rb
@@ -2,6 +2,6 @@
 
 class RemoveRootCollectionIdFromAuthorities < ActiveRecord::Migration[8.0]
   def change
-    remove_column :authorities, :root_collection_id, :integer
+    remove_column :authorities, :root_collection_id, :integer, if_exists: true
   end
 end
