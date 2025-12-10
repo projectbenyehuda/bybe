eae1ca1ebe091be464650649b36d7802cb491743
Asaf Bartov <asaf.bartov@gmail.com>
2025-12-10 05:03:03 +0200

    Made migrations tolerant of existing changes

--- db/migrate/20250819084721_add_authority_id_to_lex_person.rb
+++ db/migrate/20250819084721_add_authority_id_to_lex_person.rb
@@ -2,7 +2,7 @@
 
 class AddAuthorityIdToLexPerson < ActiveRecord::Migration[7.2]
   def change
-    remove_column :lex_people, :person_id, :integer
-    add_belongs_to :lex_people, :authority, foreign_key: true, type: :integer, index: true, null: true
+    remove_column :lex_people, :person_id, :integer, if_exists: true
+    add_belongs_to :lex_people, :authority, foreign_key: true, type: :integer, index: true, null: true, if_not_exists: true
   end
 end
--- db/migrate/20251029093451_drop_about_from_lex_people.rb
+++ db/migrate/20251029093451_drop_about_from_lex_people.rb
@@ -2,6 +2,6 @@
 
 class DropAboutFromLexPeople < ActiveRecord::Migration[8.0]
   def change
-    remove_column :lex_people, :about, type: :text
+    remove_column :lex_people, :about, type: :text, if_exists: true
   end
 end
