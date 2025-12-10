623ebf9b23d437195cfd5e0754babe155c9588f8
Asaf Bartov <asaf.bartov@gmail.com>
2025-12-10 05:03:03 +0200

    Made migrations tolerant of existing changes

--- db/migrate/20250624184852_add_event_to_year_totals.rb
+++ db/migrate/20250624184852_add_event_to_year_totals.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class AddEventToYearTotals < ActiveRecord::Migration[6.1]
   def change
-    add_column :year_totals, :event, :string
+    add_column :year_totals, :event, :string, if_not_exists: true
 
     execute "update year_totals set event = 'view'"
 
