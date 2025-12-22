ec219c6afb83e9e737693fb694ccdb43bb7aa2c1
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-12-22 15:52:05 +0500

    Fixed couple of issues in Files/migration process: - wrapped import Job with Chewy strategy (as we have index for LexPerson now) - allowed to re-analyze errored files

--- app/sidekiq/lexicon/ingest_file.rb
+++ app/sidekiq/lexicon/ingest_file.rb
@@ -7,21 +7,23 @@ module Lexicon
 
     def perform(lex_file_id)
       lex_file = LexFile.find(lex_file_id)
-      LexEntry.transaction do
-        if lex_file.entrytype_person?
-          IngestPerson.call(lex_file)
-        elsif lex_file.entrytype_text?
-          IngestPublication.call(lex_file)
-        else
-          raise "unsupported entrytype: #{lex_file.entrytype}"
+      Chewy.strategy(:atomic) do
+        LexEntry.transaction do
+          if lex_file.entrytype_person?
+            IngestPerson.call(lex_file)
+          elsif lex_file.entrytype_text?
+            IngestPublication.call(lex_file)
+          else
+            raise "unsupported entrytype: #{lex_file.entrytype}"
+          end
         end
       end
     rescue StandardError => e
+      puts e.message
+      puts e.backtrace.join("\n")
       lex_file&.error_message = e.message
       lex_file&.save!
       lex_file&.lex_entry&.status_error!
-      # puts e.message
-      # puts e.backtrace.join("\n")
     end
   end
 end
