596dfc030e8fb7dbdedba30933b5613c8f703f2e
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-12-26 03:23:19 +0500

    modified LexLinks UI to allow links CRUD for LexPublications too (previously it was only for LexPerson)

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,6 @@ Bybeconv::Application.routes.draw do
 
         resources :authors, controller: 'citation_authors', only: %i(index create)
       end
-      resources :links, shallow: true, except: %i(show)
     end
 
     resources :citation_authors, only: %i(update destroy)
@@ -79,6 +78,7 @@ Bybeconv::Application.routes.draw do
     resources :publications, only: %i(edit update new create)
     resources :entries, except: %i(update new create) do
       resources :attachments, only: %i(index create destroy)
+      resources :links, shallow: true, except: %i(show)
     end
     resources :files, only: :index do
       member do
