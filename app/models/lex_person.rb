a055c708bee2d91235f07b99a83f57bfd0b9c763
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-12-26 03:23:19 +0500

    modified LexLinks UI to allow links CRUD for LexPublications too (previously it was only for LexPerson)

--- app/models/lex_person.rb
+++ app/models/lex_person.rb
@@ -3,19 +3,16 @@
 # Person from Lexicon
 class LexPerson < ApplicationRecord
   include LifePeriod
+  include LexEntryItem
 
   update_index('lex_people_autocomplete') { self }
 
   enum :gender, { male: 0, female: 1, other: 2, unknown: 3 }
 
   has_many :citations, inverse_of: :person, class_name: 'LexCitation', dependent: :destroy
-  has_one :entry, as: :lex_item, class_name: 'LexEntry', dependent: :destroy
-  has_many :links, as: :item, dependent: :destroy, class_name: 'LexLink', inverse_of: :item
   has_many :citation_authors, class_name: 'LexCitationAuthor', dependent: :nullify
 
   belongs_to :authority, optional: true # link to an Authority record representing this person in BYP
-
-  accepts_nested_attributes_for :entry, update_only: true
 
   def intellectual_property
     copyrighted? ? 'copyrighted' : 'public_domain'
