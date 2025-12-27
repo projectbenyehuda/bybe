230b8751fdfe50a7c124f755d1eed48aab1099d9
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-12-27 22:23:19 +0500

    Updated MigrateAttachment service to attach files to LexEntry based on legacy filename

--- spec/services/lexicon/migrate_attachment_spec.rb
+++ spec/services/lexicon/migrate_attachment_spec.rb
@@ -10,12 +10,46 @@ describe Lexicon::MigrateAttachment do
   context 'when proper local path is provided', vcr: { cassette_name: 'lexicon/mirate_attachment/03127-image002' } do
     let(:src) { '03127-files/image002.jpg' }
 
-    it 'loads resource in entry attachment and returns path to it' do
-      expect { call }.to change { lex_entry.attachments.count }.by(1).and change { lex_entry.legacy_links.count }.by(1)
+    context 'when LexFile with name listed in src exists' do
+      let!(:lex_file) { create(:lex_file, fname: '03127.php') }
 
-      expect(lex_entry.legacy_links.last.old_path).to eq('03127-files/image002.jpg')
-      expect(lex_entry.legacy_links.last.new_path).to be_present
-      expect(call).not_to be_nil
+      it 'attaches resource to the lex entry associated with the LexFile and returns path to it' do
+        entry = lex_file.lex_entry
+        expect { call }.to change { entry.attachments.count }.by(1)
+                             .and change { entry.legacy_links.count }.by(1)
+        link = entry.legacy_links.last
+        expect(link.old_path).to eq('03127-files/image002.jpg')
+        expect(link.new_path).to be_present
+        expect(call).to eq(link.new_path)
+      end
+    end
+
+    context 'when LexFile with name listed in src does not exists' do
+      it 'loads resource in provided entry attachment and returns path to it' do
+        expect { call }.to change { lex_entry.attachments.count }.by(1).and change { lex_entry.legacy_links.count }.by(1)
+        link = lex_entry.legacy_links.last
+        expect(link.old_path).to eq('03127-files/image002.jpg')
+        expect(link.new_path).to be_present
+        expect(call).to eq link.new_path
+      end
+    end
+
+    context 'when LegacyLink for the same path already exists' do
+      let(:other_entry) { create(:lex_entry, :person) }
+      let!(:legacy_link) do
+        create(
+          :lex_legacy_link,
+          old_path: '03127-files/image002.jpg',
+          new_path: 'https://test.com',
+          lex_entry: other_entry
+        )
+      end
+
+      it 'returns new_path from existing LegacyLink' do
+        expect { call }.to not_change(ActiveStorage::Blob, :count)
+                             .and(not_change(LexLegacyLink, :count))
+        expect(call).to eq 'https://test.com'
+      end
     end
   end
 
@@ -26,9 +60,10 @@ describe Lexicon::MigrateAttachment do
     it 'loads resource in entry attachment and returns path to it' do
       expect { call }.to change { lex_entry.attachments.count }.by(1).and change { lex_entry.legacy_links.count }.by(1)
 
-      expect(lex_entry.legacy_links.last.old_path).to eq('03127-files/image002.jpg')
-      expect(lex_entry.legacy_links.last.new_path).to be_present
-      expect(call).not_to be_nil
+      link = lex_entry.legacy_links.last
+      expect(link.old_path).to eq('03127-files/image002.jpg')
+      expect(link.new_path).to be_present
+      expect(call).to eq link.new_path
     end
   end
 
