621d8fe52cb7e199b3a5ee765ccee472044e7e8b
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-01-26 17:54:30 +0500

    Files controller (#942)
    * Implemented FileController to generate user-friendly URLs and Paths for attachments in LexEntries and LexManifestations
    Updated MigrateAttachment service and UI to use those links instead of ActiveStorage permanent URLs
    Added check to AttachmentsController to prevent uploading of several files with similar names.

--- spec/requests/files_spec.rb
+++ spec/requests/files_spec.rb
@@ -20,6 +20,62 @@ describe '/files' do
       end
     end
 
+    context 'when entry is a LexEntry' do
+      let(:record_type) { 'lex' }
+      let(:record) { create(:lex_entry) }
+      let(:record_id) { record.id }
+
+      before do
+        record.attachments.attach(
+          io: StringIO.new('First'),
+          filename: 'file_1.txt',
+          content_type: 'text/plain'
+        )
+        record.attachments.attach(
+          io: StringIO.new('Second'),
+          filename: 'file_2',
+          content_type: 'text/plain'
+        )
+      end
+
+      context 'when wrong entry_id is given' do
+        let(:record_id) { record.id + 1 }
+        let(:filename) { 'file_1.txt' }
+
+        it 'fails with Not Found status' do
+          expect(call).to eq(404)
+          expect(response.body).to eq("Record not found: #{record_id}")
+        end
+      end
+
+      context 'when wrong filename is given' do
+        let(:filename) { 'missing.txt' }
+
+        it 'fails with Not Found status' do
+          expect(call).to eq(404)
+          expect(response.body).to eq('File not found: missing.txt')
+        end
+      end
+
+      context 'when correct file is requested' do
+        let(:filename) { 'file_1.txt' }
+
+        it 'redirects to the file URL' do
+          attachment = record.attachments.detect { |att| att.filename.to_s == 'file_1.txt' }
+          expect(call).to redirect_to(rails_blob_url(attachment.blob, disposition: 'attachment'))
+        end
+      end
+
+      context 'when file without extension is requested' do
+        let(:filename) { 'file_2' }
+
+        it 'redirects to the file URL' do
+          attachment = record.attachments.detect { |att| att.filename.to_s == 'file_2' }
+          expect(call).to redirect_to(rails_blob_url(attachment.blob, disposition: 'attachment'))
+        end
+      end
+    end
+
     context 'when record is a Manifestation' do
       let(:record) do
         create(:manifestation).tap do |record|
