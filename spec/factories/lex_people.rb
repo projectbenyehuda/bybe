fdc8c2eaa3ffaafc22e95d569a41d53980472ce9
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-02-13 01:44:56 +0500

    fixed broken spec

--- spec/factories/lex_people.rb
+++ spec/factories/lex_people.rb
@@ -15,7 +15,7 @@ FactoryBot.define do
       next_seqno = 1
       result = []
       works_count.times do
-        result << build(:lex_person_work, person: lex_person, seqno: next_seqno)
+        result << build(:lex_person_work, person: nil, seqno: next_seqno)
         next_seqno += 1
       end
       result
