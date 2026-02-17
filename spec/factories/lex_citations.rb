f9c4ead9ec0de53bbaacf7393624b32dd005f4fb
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-02-18 02:29:34 +0500

    Lex citations ordering (#1006)
    * added seqno column to lex_citations table
    * Add drag-and-drop ordering for LexCitations grouped by subject_title

--- spec/factories/lex_citations.rb
+++ spec/factories/lex_citations.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence(:lex_citation_seqno) { |n| n }
+
   factory :lex_citation do
     transient do
       authors_count { 1 }
@@ -11,5 +13,10 @@ FactoryBot.define do
     authors { build_list(:lex_citation_author, authors_count) }
     pages { Random.rand(1..100).to_s }
     link { Faker::Internet.url }
+
+    # Assign seqno automatically in factory (it only guranteesuniqieness of values)
+    seqno do
+      generate(:lex_citation_seqno)
+    end
   end
 end
