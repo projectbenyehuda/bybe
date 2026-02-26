49d5750b2a96d6251f9357d8bd08359980008b52
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-02-27 03:35:23 +0500

    linter fix

--- spec/services/lexicon/process_links_spec.rb
+++ spec/services/lexicon/process_links_spec.rb
@@ -13,7 +13,7 @@ describe Lexicon::ProcessLinks do
         <li><a id="link_3" href="#anchor">Local Link</a></li>
         <li><a id="link_4" href="">Empty Link</a></li>
         <li><a id="link_5" href="hbe/hbe00898.php">HBE Link</li>
-      </ul>  
+      </ul>
     HTML
   end
 
