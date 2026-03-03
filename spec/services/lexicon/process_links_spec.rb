395016f918d3e7e8aa6e1b586cf3bc4f53215f1b
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-03-03 20:26:00 +0500

    Updated Lexicon::ProcessLinks service to update existing links to old lexicon pages with a links to their locations in a new lexicon.

--- spec/services/lexicon/process_links_spec.rb
+++ spec/services/lexicon/process_links_spec.rb
@@ -13,9 +13,15 @@ describe Lexicon::ProcessLinks do
         <li><a id="link_3" href="#anchor">Local Link</a></li>
         <li><a id="link_4" href="">Empty Link</a></li>
         <li><a id="link_5" href="hbe/hbe00898.php">HBE Link</a></li>
+        <li><a id="link_6" href="12345.php">Existing Lexicon Page</a></li>
+        <li><a id="link_7" href="12345.php#תווית">Existing Lexicon Page with an Anchor</a></li>
+        <li><a id="link_8" href="98765.php">Missing Lexicon Page</a></li>
       </ul>
     HTML
   end
+
+  let!(:existing_lex_file) { create(:lex_file, :person, fname: '12345.php') }
+  let(:existing_lex_entry) { existing_lex_file.lex_entry }
 
   let(:html_doc) { Nokogiri.parse(html) }
 
@@ -36,5 +42,8 @@ describe Lexicon::ProcessLinks do
     expect(html_doc.at_css('#link_3')['href']).to eq('#anchor')
     expect(html_doc.at_css('#link_4')['href']).to eq('')
     expect(html_doc.at_css('#link_5')['href']).to eq('/lexicon/hbe/hbe00898.php')
+    expect(html_doc.at_css('#link_6')['href']).to eq("/lex/entries/#{existing_lex_entry.id}")
+    expect(html_doc.at_css('#link_7')['href']).to eq("/lex/entries/#{existing_lex_entry.id}#תווית")
+    expect(html_doc.at_css('#link_8')['href']).to eq('98765.php') # not changed as there is no matching lexicon entry
   end
 end
