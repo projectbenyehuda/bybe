aa1494fc2cce96c4355f5f2fb1228114b5931069
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-03-12 17:26:53 +0500

    Added handling for static images reused on many lexicon pages. They will be stored in a public folder instead of ActiveStorage

--- app/services/lexicon/attach_images.rb
+++ app/services/lexicon/attach_images.rb
@@ -7,6 +7,14 @@ module Lexicon
     def call(html_doc, lex_entry)
       html_doc.css('img').each do |img|
         src = img['src']
+
+        # Handling for files shared between number of entries
+        # They're stored in public/lex folder instead of ActiveStorage
+        if src.start_with?('00000_files/')
+          img['src'] = src.sub('00000_files/', '/lex/')
+          next
+        end
+
         new_src = MigrateAttachment.call(src, lex_entry)
         if new_src.present?
           img['src'] = new_src
