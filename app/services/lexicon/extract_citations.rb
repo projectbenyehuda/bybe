1ed0f9e7e4b8e28042cce41b833fbcc1ae410735
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-03-16 12:26:04 +0500

    Added handling for some special cases during citations and works parsing

--- app/services/lexicon/extract_citations.rb
+++ app/services/lexicon/extract_citations.rb
@@ -3,13 +3,15 @@
 module Lexicon
   # Service to extract citations from Lexicon entry php file
   class ExtractCitations < ApplicationService
-    CITATIONS_HEADER_MALE = 'על המחבר ויצירתו:'
-    CITATIONS_HEADER_FEMALE = 'על המחברת ויצירתה:'
+    HEADERS = ['על המחבר ויצירתו:', 'על המחברת ויצירתה:', 'על המחברות ויצירתן:'].freeze
 
     def call(html_doc)
-      header = header_element(html_doc, CITATIONS_HEADER_MALE)
-      header = header_element(html_doc, CITATIONS_HEADER_FEMALE) if header.nil?
-      return if header.nil?
+      header = nil
+      HEADERS.each do |header_title|
+        header = header_element(html_doc, header_title)
+        break if header.present?
+      end
+      return [] if header.nil?
 
       # The next element should be a 'font' tag containing all citations. Sometimes there could be one or more blank
       # paragraphs before it, so we need to skip them.
