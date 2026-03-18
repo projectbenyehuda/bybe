6b2b33fda83c546948eea3a959ef04bf4faf6853
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-03-18 22:26:48 +0500

    Added workaround for sitations when Citations are presented not by single html node, but by several nodes

---
+++ spec/data/lexicon/04443.php
@@ -0,0 +1,138 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="lexicon2.css" type="text/css">
+<!-- #BeginEditable "doctitle" -->
+<title>מתתיהו שמחה רבנר</title>
+<!-- #EndEditable -->
+</head>
+
+<!-- page header --><?php
+			// include page header
+			include "header.php";
+			?><body dir="rtl">
+<table border="0" width="100%" id="table5">
+	<!-- MSCellType="ContentBody" -->
+	<tr>
+		<td>
+		<p align="center"><font size="5" color="#FF0000">מתתיהו שמחה רבנר </font>
+		<font size="4" color="#FF0000">(1826–1894)</font></p>
+		</td>
+		<td>
+		<p align="center"><font size="2" color="#FF0000">&lt;בהכנה&gt;</font></p>
+		</td>
+		<td>
+		<p align="center" dir="ltr"><font size="5" color="#FF0000">Matthias Simcha 
+		Rabener</font></p>
+		</td>
+	</tr>
+</table>
+<p class="margin" align="justify">
+<img border="0" src="04443-files/04443-image001.jpg" width="200" height="287" align="left" hspace="10">&nbsp;&nbsp;&nbsp; 
+מתתיהו שמחה רבנר (ראבענער), סופר ורב, נולד לאביו, יהודה ליב רבנר בלמברג, גליציה 
+(כיום, לביב באוקראינה) בט״ו בשבט תרכ״ו, 23 בינואר 1826<font color="#FF0000">*</font>. 
+קיבל חינוך מסורתי בישיבה. למד עברית חדשה ושפות זרות. ב־1860 התמנה כמורה בבית הספר 
+היהודי בצ׳רנוביץ, בוקובינה. ב־1867 שימש מורה לדת בגימנסיה בסוכוב, מורביה (כיום בתחומי 
+צ׳כיה), ולאחר מכן כמנהל בית הספר היהודי בפאלטיצ׳ן ברומניה. בשנת 1869 נתמנה כמורה 
+ומנהל בית היתומים נוישולץ ביאסי. כאן הוא לקח חלק פעיל בחיים האינטלקטואלים היהודים 
+ושימש כמתורגמן של הקונסוליה האוסטרו-הונגרית, היה חבר באגודה הגרמנית לחקר המזרח בלייפציג 
+והאַלֶה, ובאגודה לחקר ארץ־ישראל בלייפציג. כמו כן היה חבר באגודה לאמנות לבוהמיה בפראג, 
+וחבר כבוד באגודות פילנתרופיות רבות.<br>
+<span lang="en-us">&nbsp;&nbsp;&nbsp; </span>לפי ידיעה בעיתון <i>הצפירה</i> מיום 
+25 באפריל 1894 מאת <a href="03816.php">מו״ח <font size="2">[הוא מרדכי 
+וייסמן־חיות]</font></a>, נפטר רבנר באותו חודש לאחר מחלה קצרה.<br>
+&nbsp;&nbsp;&nbsp; מספריו: נגינות עבר (״שירים שונים יסודתם בהררי קדש אשר העתקתי 
+משירי המליץ ... לאָרד ביראָן״. טשערנאוויטץ, תרכ״ד ; הוצאה שניה, יאסי, תרמ״ה 1884); 
+עת הזמר (״אספת מבחר שירי המשורר... שיללער ; אשר העתיק ללשון עברית בעת תקופת מאה 
+שנה לחיי שיללער... ״ (טשערנאוויץ, תרכ״ב 1862; מהדורה שניה, יאסי תרכ״ח 1868), השולמית 
+(״או שיר השירים אשר לשלמה, מקרא קודש על דרך החזיון בחמש מחזות עם תרגום אשכנזי בחרוזים 
+חדשים״ (יאסי, 1880), ספר ציונים לתורה (״תרי״ג מצות ... כאשר התפקדו מאת הרמב״ם וכל 
+מצוה מתחלת במלה דרך נוטריקון ... ״. דראהביטש, תרמ״ז), ועוד.<br>
+&nbsp;&nbsp;&nbsp; <font color="#FF0000">*</font><font size="2"> לפי
+<span lang="en-us">Wininger </span>נולד ב־28 בינואר 1829.</font> </p>
+<table border="0" width="35%" align="left" id="table6">
+	<tr>
+		<td><a target="_blank" href="https://www.nli.org.il">
+		<img border="0" src="00000_files/nli.png" width="150" height="45"><br>
+		מתתיהו שמחה רבנר בספריה הלאומית</a> </td>
+		<td><a target="_blank" href="https://benyehuda.org/author/2575">
+		<img border="0" src="00000_files/Ben-Yehuda-s.jpg" width="160" height="35"><br>
+		יצירות מתתיהו שמחה רבנר בפרויקט בן־יהודה</a></td>
+	</tr>
+</table>
+<p class="margin" align="justify"><font size="2">מקורות: אוצר הספרות, שנה שניה (תרמ״ח 
+1888), <a target="_blank" href="https://he.wikipedia.org/wiki/מתתיהו_שמחה_רבנר">ויקיפדיה</a>
+<span dir="ltr">S. Wininger. Grosse jüdische National-Biographie (1925),<br> 
+<a target="_blank" href="https://jewishencyclopedia.com/articles/12503-rabener-mattithiah-simhah-b-judah-lob">Jewish 
+encyclopedia (1906)</a>,
+<a target="_blank" href="https://ru.wikisource.org/wiki/ЕЭБЕ/Рабенер,_Матиас_Симха">Еврейская энциклопедия Брокгауза и Ефрона</a> </span></font></p>
+<font size="4" color="#0000FF"><a name="Books">ספריו:</a></font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
+<font size="4" color="#0000FF">עריכה:</font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+</ul>
+<font size="4" color="#0000FF"><a name="Bib.">על המחבר ויצירתו:</a></font>
+<font size="2"><font color="#FF0000"></font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+</ul>
+<font color="#FF0000"></font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+	<li><b>קאָפלער, משה.</b>&nbsp;
+	<a target="_blank" href="99995-files/99995119/02/99995119-02-4-294-296.pdf">
+	תולדות החכם מו״ה מתתיהו שמחה ראבענער.</a>&nbsp; <u>אוצר הספרות</u>, שנה שניה 
+	(תרמ״ח 1888), עמ׳ 294–296.</li>
+</font>
+	<li class="left-to-right"><b>Herşcovici, Lucian-Zeev.</b>
+	<a target="_blank" href="04443-files/04443200.pdf">Matityahu Simcha Rabener şi revista Zimrath Haaretz : o pagină din istoria mişcării de <i>Haskala</i> din Romănia.</a> in: 
+	<b>Istorie și memorie evreiească</b> : volum omagial dedicat doamnei dr. Lya Benjamin / Centrul pentru Studiul Istoriei Evreilor din România ; editorl, Anca Ciuciu, Camelia Crăciun 
+	(București<span lang="en-us"> </span>: Hasefer, 2011), pages 59–70.</li>
+</ul>
+<font color="#FF0000"></font>
+<font size="2">
+<ul style="MARGIN-TOP: 0in" type="circle">
+</ul>
+<font color="#FF0000"></font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+</ul>
+<font color="#FF0000"></font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+</ul>
+</font><font size="4" color="#0000FF"><a name="links">קישורים:</a></font>
+<font color="#FF0000"></font>
+<ul style="MARGIN-TOP: 0in" type="circle">
+</ul>
+<div style="border-top-style: solid; border-top-width: 1px; padding-top: 1px">
+	<table align="left" border="0" width="85%">
+		<tr>
+			<td dir="ltr"><font size="2">OpenLibrary –
+			<a target="_blank" href="https://openlibrary.org/authors/OL13430890A">
+			OL13430890A</font></a></td>
+			<td dir="ltr"><font size="2">Wikidata –
+			<a target="_blank" href="https://www.wikidata.org/entity/Q12409731">
+			Q12409731</font></a></td>
+			<td dir="ltr"><font size="2">NLI –
+			<a target="_blank" href="https://www.nli.org.il/he/authorities/987007519078905171">
+			987007519078905171</font></a></td>
+			<!--	<td dir="ltr"><font size="2">NLI – <a target="_blank" href="http://olduli.nli.org.il/F/?func=direct&local_base=nlx10&doc_number=001432054">001432054</font></td> -->
+			<td dir="ltr"><font size="2">LC –
+			<a target="_blank" href="https://id.loc.gov/authorities/nb2009008724/">
+			nb2009008724</font></a></td>
+			<td dir="ltr"><font size="2">VIAF –
+			<a target="_blank" href="https://viaf.org/viaf/141147266569335480503/">
+			141147266569335480503</font></a></td>
+		</tr>
+	</table>
+	<font size="2">עודכן לאחרונה: 24 באפריל 2025</font> </div>
+<!-- page footer --><?php
+			// include page footer
+			include "footerm.php";
+			?>
+
+</body>
+
+</html>
