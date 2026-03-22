9b330cf9178fc1c55e84a5875a73ac3bdff35d59
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-03-22 14:15:58 +0500

    Used JS view instead of custom jQuery handler

--- app/controllers/lexicon/files_controller.rb
+++ app/controllers/lexicon/files_controller.rb
@@ -35,6 +35,10 @@ module Lexicon
     end
 
     def migrate
+      # From the stale page we can try to migrate file already being migrated or already migrated
+      # In this case we simply re-render row
+      return unless @lex_file.lex_entry.status_raw? || @lex_file.lex_entry.status_error?
+
       if @lex_file.error_message.present?
         @lex_file.update!(error_message: nil)
       end
@@ -45,13 +49,8 @@ module Lexicon
       lex_entry.status_migrating!
 
       # Cleaning up any existing LexItem before re-ingesting
-      lex_item.destroy! if lex_item.present?
+      lex_item&.destroy!
       Lexicon::IngestFile.perform_async(@lex_file.id)
-
-      # Reload to capture the state after scheduling (lex_entry status is now migrating).
-      # The async job's result won't be reflected here.
-      @lex_file.reload
-      render partial: 'file_row', locals: { lf: @lex_file }
     end
 
     private
