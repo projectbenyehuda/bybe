d85b7ca3078965e09b8467cc0486c77190e7623c
Asaf Bartov <asaf.bartov@gmail.com>
2026-03-22 03:02:53 +0200

    read dotenv in production too

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/bybe/bybeconv_staging"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/s3.yml", "config/constants.yml", "config/storage.yml", "config/chewy.yml", "config/puma.rb"
+append :linked_files, "config/database.yml", "config/s3.yml", "config/constants.yml", "config/storage.yml", "config/chewy.yml", "config/puma.rb", ".env.production"
 
 append :linked_dirs, '.bundle', 'log', 'tmp/cache', 'public/system', 'tmp/pids', 'tmp/sockets'
 
@@ -55,11 +55,4 @@ namespace :debug do
     end
   end
 end
-
-task :load_remote_environment do
-  on roles(:app) do
-    set :default_environment, Dotenv::Parser.call(capture("cat #{shared_path}/.env.production"))
-  end
-end
-before 'deploy:starting', 'load_remote_environment'
 
