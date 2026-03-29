a69366803119172d25756999d2104bafd15330ca
Asaf Bartov <asaf.bartov@gmail.com>
2026-03-29 10:25:53 +0300

    Block Alibaba cloud

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -9,9 +9,9 @@ class Rack::Attack
     # Check if user is logged in via session
     req.session['user_id'].present?
   end
-  BAD_IPS = ["147.189.171.165"]
+  BAD_IPS = ["147.189.171.165", '47.80.0.0/13', '47.76.0.0/14','47.74.0.0/15'] # Alibaba cloud, which has been pounding our server inconsiderately (and probably uselessly)
   Rack::Attack.blocklist "Block IPs from Environment Variable" do |req|
-    BAD_IPS.include?(req.ip) || req.path.index('&gt&gt')
+    BAD_IPS.include?(req.ip)
   end
   # Throttle requests to 100 requests per 3 minutes per IP
   throttle('req/ip', limit: 100, period: 3.minutes) do |req|
