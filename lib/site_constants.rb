c0fb826a6f526ca2175cacd487b1f5a494fc2add
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-04-03 17:55:00 +0500

    Moved `youtube_channel_id` constant from constants.yml to SiteConstants

--- lib/site_constants.rb
+++ lib/site_constants.rb
@@ -5,4 +5,5 @@ module SiteConstants
   OUR_PUBLISHER = 'פרויקט בן-יהודה'
   OUR_PLACE_OF_PUBLICATION = 'benyehuda.org'
   TAGGING_POLICY_URL = 'https://benyehuda.org/page/tagging'
+  YOUTUBE_CHANNEL_ID = 'UClsusG2EWu45WZ-yNJsdFAw' # Ben-Yehuda YouTube channel ID
 end
