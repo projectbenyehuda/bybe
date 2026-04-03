306e38c8498bc386c185f90425b3d17b34062127
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-04-03 13:33:55 +0500

    moved our_publisher and our_place_of_publication constants from constants.yml to ruby constants in a new SiteConstant ruby module

---
+++ lib/site_constants.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# This module contains constants related to the site.
+module SiteConstants
+  OUR_PUBLISHER = 'פרויקט בן-יהודה'
+  OUR_PLACE_OF_PUBLICATION = 'benyehuda.org'
+end
