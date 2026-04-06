341e6d8a1b494fc3b22569c73ced7209d2a0a006
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-04-07 02:51:16 +0500

    linter fixes

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,6 @@
-app_dir = File.expand_path("../..", __FILE__)
+# frozen_string_literal: true
+
+app_dir = File.expand_path('..', __dir__)
 shared_dir = "#{app_dir}"
 rackup(File.expand_path('../config.ru', __dir__))
 if ENV['RACK_ENV'] == 'production'
@@ -12,18 +14,18 @@ if ENV['RACK_ENV'] == 'production'
     require 'puma_worker_killer'
     PumaWorkerKiller.config do |config|
       config.ram           = 4096 # mb
-      config.frequency     = 60    # seconds
+      config.frequency     = 60 # seconds
       config.percent_usage = 0.98
-      #config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds, or 12.hours if using Rails
-      #config.reaper_status_logs = true # setting this to false will not log lines like:
+      # config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds, or 12.hours if using Rails
+      # config.reaper_status_logs = true # setting this to false will not log lines like:
       # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
-  
-      #config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
-      #config.rolling_pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed by rolling restart" }
+
+      # config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
+      # config.rolling_pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed by rolling restart" }
     end
     PumaWorkerKiller.start
   end
-  
+
 else
   workers 3
   environment 'development'
@@ -33,8 +35,8 @@ end
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments.
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+plugin :solid_queue if ENV['SOLID_QUEUE_IN_PUMA']
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+pidfile ENV['PIDFILE'] if ENV['PIDFILE']
