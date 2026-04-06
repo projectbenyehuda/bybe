7846c58efd2b1e75e915a5cbe1a9aeb316cfb001
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-04-06 13:50:00 +0500

    Removed settings, gems and leftovers of code used to log in with Twitter account

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.configuration.constants['google_oauth_client_id'], Rails.configuration.constants['google_oauth_client_secret']
-  provider :twitter, Rails.configuration.constants['twitter_consumer_key'], Rails.configuration.constants['twitter_consumer_secret']
   provider :developer if Rails.env == 'development'
 end
 #OmniAuth.config.allowed_request_methods = [:post, :get]
--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -4,11 +4,19 @@ describe SessionController do
   describe 'create' do
     before(:all) do
       OmniAuth.config.test_mode = true
-      OmniAuth.config.add_mock(:twitter, { uid: 'UID', info: { email: 'test@test.com', name: 'Asaf' }, credentials: { token: 'TOKEN' } })
+      OmniAuth.config.add_mock(
+        :google_oauth2,
+        { uid: 'UID', info: { email: 'test@test.com', name: 'Asaf' }, credentials: { token: 'TOKEN' } }
+      )
     end
 
     before do
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    end
+
+    after(:all) do
+      OmniAuth.config.test_mode = false
+      OmniAuth.config.mock_auth[:google_oauth2] = nil
     end
 
     subject { post :create }
@@ -20,7 +28,13 @@ describe SessionController do
         it 'creates new User record and updates BaseUser record' do
           expect { subject }.to change { User.count }.by(1).and change { BaseUser.count }.by(0)
           u = User.order(id: :desc).first
-          expect(u).to have_attributes email: 'test@test.com', name: 'Asaf', provider: 'twitter', uid: 'UID', oauth_token: 'TOKEN'
+          expect(u).to have_attributes(
+            email: 'test@test.com',
+            name: 'Asaf',
+            provider: 'google_oauth2',
+            uid: 'UID',
+            oauth_token: 'TOKEN'
+          )
 
           base_user.reload
           expect(base_user).to have_attributes(user: u, session_id: nil)
@@ -32,7 +46,13 @@ describe SessionController do
         it 'creates new User record and new BaseUser record' do
           expect { subject }.to change { User.count }.by(1).and change { BaseUser.count }.by(1)
           u = User.order(id: :desc).first
-          expect(u).to have_attributes email: 'test@test.com', name: 'Asaf', provider: 'twitter', uid: 'UID', oauth_token: 'TOKEN'
+          expect(u).to have_attributes(
+            email: 'test@test.com',
+            name: 'Asaf',
+            provider: 'google_oauth2',
+            uid: 'UID',
+            oauth_token: 'TOKEN'
+          )
 
           base_user = BaseUser.order(id: :desc).first
           expect(base_user).to have_attributes(user: u, session_id: nil)
@@ -42,7 +62,7 @@ describe SessionController do
     end
 
     context 'when existing User sign in' do
-      let!(:user) { create(:user, provider: 'twitter', uid: 'UID') }
+      let!(:user) { create(:user, provider: 'google_oauth2', uid: 'UID') }
       let!(:existing_base_user) { create(:base_user, user: user) }
       context 'when anonymous BaseUser record exists' do
         let!(:base_user) { BaseUser.create(session_id: session.id.private_id) }
@@ -50,7 +70,13 @@ describe SessionController do
         it 'uses existing User record and deletes anonymous BaseUser' do
           expect { subject }.to change { User.count }.by(0).and change { BaseUser.count }.by(-1)
           user.reload
-          expect(user).to have_attributes email: 'test@test.com', name: 'Asaf', provider: 'twitter', uid: 'UID', oauth_token: 'TOKEN'
+          expect(user).to have_attributes(
+            email: 'test@test.com',
+            name: 'Asaf',
+            provider: 'google_oauth2',
+            uid: 'UID',
+            oauth_token: 'TOKEN'
+          )
 
           # If user exists we drop BaseUser from anonymous session because existing user should already have one
           expect { base_user.reload }.to raise_error(ActiveRecord::RecordNotFound)
@@ -64,16 +90,17 @@ describe SessionController do
         it 'uses existing User record' do
           expect { subject }.to change { User.count }.by(0).and change { BaseUser.count }.by(0)
           user.reload
-          expect(user).to have_attributes email: 'test@test.com', name: 'Asaf', provider: 'twitter', uid: 'UID', oauth_token: 'TOKEN'
+          expect(user).to have_attributes(
+            email: 'test@test.com',
+            name: 'Asaf',
+            provider: 'google_oauth2',
+            uid: 'UID',
+            oauth_token: 'TOKEN'
+          )
           # ensuring existing BaseUser was kept
           expect(existing_base_user.user_id).to eq user.id
         end
       end
-    end
-
-    after(:all) do
-      OmniAuth.config.test_mode = false
-      OmniAuth.config.mock_auth[:twitter] = nil
     end
   end
 end
