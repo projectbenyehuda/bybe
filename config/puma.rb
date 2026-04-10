c3a2e277466451f147b8cdd24635e2af38509a01
Asaf Bartov <asaf.bartov@gmail.com>
2026-04-10 03:12:01 +0000

    prod settings

--- config/puma.rb
+++ config/puma.rb
@@ -13,11 +13,11 @@ if ENV['RACK_ENV'] == 'production'
   before_fork do
     require 'puma_worker_killer'
     PumaWorkerKiller.config do |config|
-      config.ram           = 4096 # mb
-      config.frequency     = 60 # seconds
+      config.ram           = 7096 # mb
+      config.frequency     = 60    # seconds
       config.percent_usage = 0.98
-      # config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds, or 12.hours if using Rails
-      # config.reaper_status_logs = true # setting this to false will not log lines like:
+      config.rolling_restart_frequency = 1 * 3600 # 12 hours in seconds, or 12.hours if using Rails
+      #config.reaper_status_logs = true # setting this to false will not log lines like:
       # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
 
       # config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
