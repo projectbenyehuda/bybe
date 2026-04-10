bac17c5f857233dedad12f90584e5e1195acbb88
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-04-10 23:56:28 +0500

    added missing event names (#1153)

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -5,9 +5,27 @@ module Ahoy
   class Event < ApplicationRecord
     include Ahoy::QueryMethods
 
-    ALLOWED_NAMES = %w(view download page_view search clicked_tag donev_text_footban donev_banner donev_sidebox_homepage
-                       donev_menu donev_mobile_menu donev_top_bar donev_mobile_top_banner donev_mobile_top_banner_scrolled
-                       donev_footban donev_footban_mobile).freeze
+    ALLOWED_NAMES = %w(
+      view
+      download
+      page_view
+      search
+      clicked_tag
+      donev_text_footban
+      donev_banner
+      donev_sidebox_homepage
+      donev_menu
+      donev_mobile_menu
+      donev_top_bar
+      donev_mobile_top_banner
+      donev_mobile_top_banner_scrolled
+      donev_footban
+      donev_footban_mobile
+      donev_footer
+      donev_bannmsg
+      donev_text_footban_mobile
+      donev_sidebox
+    ).freeze
 
     self.table_name = 'ahoy_events'
 
