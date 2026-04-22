4ff37fce4dd4c317f881b44b76abb38fff621b9c
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-04-22 17:01:59 +0500

    fixed assets_compilation? check to not return true in other rake tasks

--- lib/deployment_helpers.rb
+++ lib/deployment_helpers.rb
@@ -5,6 +5,15 @@ module DeploymentHelpers
   # require external services (e.g. ElasticSearch) to be available.
   # This method can be used to skip some initialization when we precompile assets.
   def self.assets_compilation?
-    defined?(::Rake::SprocketsTask)
+    # Check if we're in assets group (set by rails assets:precompile)
+    return true if ENV['RAILS_GROUPS'].to_s.include?('assets')
+
+    # Fallback to checking rake tasks
+    if defined?(Rake.application)
+      Rake.application.top_level_tasks.any? do |task|
+        task.to_s == 'assets:precompile' ||
+          task.to_s.start_with?('assets:precompile')
+      end
+    end
   end
 end
