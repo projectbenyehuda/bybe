2b4ca04083c276375dd255bda352819e36f3fd55
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-04-23 20:42:20 +0500

    Dockerize staging/production (#1175)
    - Created Dockerfile to generate production-ready docker images.
    - Added thruster gem to production env, and used it to serve https from docker image
    - Removed puma_worker_killer and puma_daemon gems
    - removed double inclusion of adressable gem in a Gemfile
    - Updated app initializers to allow assets precompilation if some ENV variables are missing (see DeploymentHelper.assets_compilation? method and its usage)
    - removed dotenv gem from production config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,12 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV.fetch('GOOGLE_OAUTH_CLIENT_ID'), ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET')
-  provider :developer if Rails.env == 'development'
+# frozen_string_literal: true
+
+require 'deployment_helpers'
+
+unless DeploymentHelpers.assets_compilation?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, ENV.fetch('GOOGLE_OAUTH_CLIENT_ID'), ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET')
+    provider :developer if Rails.env == 'development'
+  end
+  #OmniAuth.config.allowed_request_methods = [:post, :get]
+  OmniAuth.config.allowed_request_methods = [:post]
 end
-#OmniAuth.config.allowed_request_methods = [:post, :get]
-OmniAuth.config.allowed_request_methods = [:post]
