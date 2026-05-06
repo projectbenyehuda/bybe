ac9f12374b3d88a4e7cc911a23428e6b0a3299a2
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-06 23:38:53 +0500

    extracted regexps used to define lex_entry type from a filename to LexFile model

--- app/models/lex_file.rb
+++ app/models/lex_file.rb
@@ -2,6 +2,10 @@
 
 # Model representing legacy lexicon file
 class LexFile < ApplicationRecord
+  PERSON_REGEXP = /^\d{5}\.php$/
+  PUBLICATION_REGEXP = /^0\d\d\d\d.?\d\d\d\.php$/
+  BIB_REGEXP = /^999.+\.php$/
+
   enum :status,
        {
          unclassified: 0,
@@ -28,5 +32,30 @@ class LexFile < ApplicationRecord
     self.error_message ||= ''
     self.error_message += "\n" if self.error_message.present?
     self.error_message += message
+  end
+
+  def self.person_filename?(fname)
+    entrytype_from_filename(fname) == 'person'
+  end
+
+  def self.publication_filename?(fname)
+    entrytype_from_filename(fname) == 'text'
+  end
+
+  def self.bib_filename?(fname)
+    entrytype_from_filename(fname) == 'bib'
+  end
+
+  def self.entrytype_from_filename(fname)
+    case fname.strip
+    when LexFile::BIB_REGEXP
+      'bib'
+    when LexFile::PERSON_REGEXP
+      'person'
+    when LexFile::PUBLICATION_REGEXP
+      'text'
+    else
+      'unknown'
+    end
   end
 end
