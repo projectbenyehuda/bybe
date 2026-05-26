2130b46eaf3c28724402500379fac60836f10dea
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-26 22:45:05 +0500

    Skip mailer initialization if it is an assets compilation rake task.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
+if Rails.env.production? && !DeploymentHelpers.assets_compilation?
   ActionMailer::Base.smtp_settings = {
     address: 'smtp-relay.gmail.com',
     domain: ENV.fetch('APP_HOSTNAME', 'benyehuda.org')
