5fa9e1f71f03309a42ead24b96a3ca5ef8e53225
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-26 19:30:13 +0500

    Added specs for LexFile.log_error method

--- spec/models/lex_file_spec.rb
+++ spec/models/lex_file_spec.rb
@@ -30,4 +30,41 @@ describe LexFile do
       it { is_expected.to be false }
     end
   end
+
+  describe '.log_error' do
+    subject { file.error_message }
+
+    let(:file) { create(:lex_file, error_message: error_message) }
+
+    let(:error_message) { nil }
+
+    context 'when error_message is empty' do
+      before do
+        file.log_error('Error message')
+      end
+
+      it { is_expected.to eq('Error message') }
+    end
+
+    context 'when error_message is not empty' do
+      let(:error_message) { 'Old error message' }
+
+      before do
+        file.log_error('Error message')
+      end
+
+      it { is_expected.to eq("Old error message\nError message") }
+    end
+
+    context 'when method is called twice' do
+      let(:error_message) { 'Old error message' }
+
+      before do
+        file.log_error('First')
+        file.log_error('Second')
+      end
+
+      it { is_expected.to eq("Old error message\nFirst\nSecond") }
+    end
+  end
 end
