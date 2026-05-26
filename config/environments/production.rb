1c6b5cc5ba4920a5709a91788fbc3d943b034041
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-26 23:08:24 +0500

    Skip default host settings if it is an assets compilation rake task.
    made APP_HOSTNAME env variable mandatory.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require 'deployment_helpers'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -95,6 +96,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  routes.default_url_options = { host: ENV.fetch('APP_HOSTNAME', 'benyehuda.org'), protocol: 'https' }
-  config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOSTNAME', 'benyehuda.org') }
+  unless DeploymentHelpers.assets_compilation?
+    routes.default_url_options = { host: SiteConstants::APP_HOSTNAME, protocol: 'https' }
+    config.action_mailer.default_url_options = { host: SiteConstants::APP_HOSTNAME }
+  end
 end
