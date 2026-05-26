f0aca653faee197d2afa902a8fd8510dd336437f
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-26 23:27:15 +0500

    added missing require for SiteConstants module

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 require "active_support/core_ext/integer/time"
 require 'deployment_helpers'
+require 'site_constants'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
