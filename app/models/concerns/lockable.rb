e7ba9a4fe7344e6eb7b71deef063397628c44b7b
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-06-01 14:04:45 +0500

    added information about who and when locked entry to verification screen

--- app/models/concerns/lockable.rb
+++ app/models/concerns/lockable.rb
@@ -5,6 +5,7 @@
 # Locks also should expire after given timeout period
 module Lockable
   extend ActiveSupport::Concern
+  include ActionView::Helpers::DateHelper
 
   LOCK_TIMEOUT_IN_SECONDS = 60 * 15 # 15 minutes
 
@@ -34,5 +35,13 @@ module Lockable
 
   def release_lock!
     update_columns(locked_at: nil, locked_by_user_id: nil) # we deliberately skip validations here
+  end
+
+  def locked_by_human
+    I18n.t('lockable.locked_by_human', name: locked_by_user.name)
+  end
+
+  def locked_at_human
+    I18n.t('lockable.locked_at_human', minutes_ago: distance_of_time_in_words(Time.zone.now, locked_at))
   end
 end
