d9c2cad1a49f5d7bf02d7b12994d070516ad643b
Asaf Bartov <asaf.bartov@gmail.com>
2026-06-21 21:00:27 +0300

    Fixed biography parsing

--- app/services/html_to_markdown.rb
+++ app/services/html_to_markdown.rb
@@ -6,10 +6,13 @@ class HtmlToMarkdown < ApplicationService
     return '' if html.blank?
 
     PandocRuby.convert(
-      html.strip.gsub(/\u00A0|&nbsp;/, '<p />').gsub(%r{<br>|<br/>}, "\n\n"),
+      html.strip
+          .gsub(%r{<br\s*/?>\n?\s*(?:&nbsp;|\u00A0)+}i, '</p><p>')
+          .gsub(/\u00A0|&nbsp;/, ' '),
       M: 'dir=rtl',
       from: :html,
       to: 'markdown_mmd-pipe_tables+raw_html'
     ).force_encoding('UTF-8')
+     .gsub(/^(\d+)\. /, "\\1\\\\. ")
   end
 end
