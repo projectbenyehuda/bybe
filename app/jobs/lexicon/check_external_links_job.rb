ff15b4823470d22229007a283ef908f8f5955b90
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-07-06 16:58:20 +0600

    Sidekiq to active job (#1401)
    Converted Sidekiq worker classes to ActiveJob jobs

--- app/jobs/lexicon/check_external_links_job.rb
+++ app/jobs/lexicon/check_external_links_job.rb
@@ -4,9 +4,7 @@ module Lexicon
   # Sidekiq job that checks all external links for a given LexEntry.
   # Runs asynchronously after ingestion so that the ingestion job itself
   # is not delayed by per-link network round-trips.
-  class CheckExternalLinksJob
-    include Sidekiq::Job
-
+  class CheckExternalLinksJob < ApplicationJob
     def perform(lex_entry_id)
       lex_entry = LexEntry.includes(:lex_item).find(lex_entry_id)
       CheckExternalLinks.call(lex_entry)
