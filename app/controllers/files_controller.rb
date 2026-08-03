9798db1809690d39d1cab28380da6953f19137f2
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2026-08-04 04:11:05 +0500

    Modified FilesController to cache direct service urls instead of rails_blob_url (#1505)

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,8 +5,13 @@
 # Manifestations and StaticPages as well.
 # One limitation of this is that we assume all attachments attached to a given model are unique by filename.
 class FilesController < ApplicationController
+  # To generate urls with DiskStorage in development/test environment
+  include ActiveStorage::SetCurrent unless Rails.env.production?
+
   skip_before_action :set_paper_trail_whodunnit
   skip_before_action :set_base_user # Skip user initialization as it is a public URL
+
+  CACHE_EXPIRATION_TIME = 2.hours
 
   # URL format: /files/:record_type/:record_id/:filename
   def download
@@ -30,21 +35,24 @@ class FilesController < ApplicationController
       return
     end
 
-    redirect_to redirect_url
+    redirect_to redirect_url, allow_other_host: true
   end
 
   private
 
   def cached_redirect_urls(record_class, record_id)
     cache_key = DownloadLink.redirect_urls_cache_key(record_class, record_id)
-    Rails.cache.fetch(cache_key, expires_in: 2.hours, race_condition_ttl: 10.seconds) do
+    Rails.cache.fetch(cache_key, expires_in: CACHE_EXPIRATION_TIME, race_condition_ttl: 10.seconds) do
       record = record_class.find_by(id: record_id)
 
       result = {}
       record&.downloadable_attachments&.each do |attachment|
         blob = attachment.blob
         disposition = blob.image? ? 'inline' : 'attachment'
-        result[attachment.filename.to_s] = rails_blob_url(blob, disposition: disposition)
+        result[attachment.filename.to_s] = blob.url(
+          disposition: disposition,
+          expires_in: CACHE_EXPIRATION_TIME + 1.minute
+        )
       end
       result
     end
