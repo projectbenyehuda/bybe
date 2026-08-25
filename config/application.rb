242125c705b44f4b9591aa5e51c9c30a55cb9939
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-08-25 14:38:46 +0500

    removed capistrano

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Bybeconv
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks capistrano templates))
+    config.autoload_lib(ignore: %w(assets tasks templates))
 
     # Configuration for the application, engines, and railties goes here.
     #
